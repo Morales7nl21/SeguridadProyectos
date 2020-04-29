1a8b80e9fa1892be5133492c7ffb3e8614ce8e93
Morales7nl21clear <thedjdave98@gmail.com>
2020-04-29 00:59:02 -0500

    ...

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -36,12 +36,16 @@ def scan (ip):
 
     # Mandar un paquete con un mac customizado lo permite el metodo srp
     # retorna una pareja de dos listas, el primer elemento de la lista de parejas es
-    # (packet sent, answer) y el segundo de la lista son los paquetes sin responder.
+    # (packet sent, answer) y el segundo de la lista son los paquetes sin responder, ya que
+    # no nos interesan los unanswered se pueden quitar pero se tiene que referenciar a solo el
+    # elemento 1 es decir 0 de la lista.
 
-    answered, unanswered = scapy.srp(arp_request_broadcast,timeout = 1)
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
 
-    # Imprimimos lo que nos dio de la variable combinada el metodo summary.
-    print(answered.summary())
+
+
+    # T1 Imprimimos lo que nos dio de la variable combinada el metodo summary.
+    # print(answered_list.summary())
     # arp_request_broadcast.show()
 
 
@@ -49,6 +53,21 @@ def scan (ip):
     # !! El metodo ls lista los metodos que oueden ser modificados, como ip destino para hacer el broadcast, mac, etc.
     # scapy.ls(scapy.ARP())
     # scapy.ls(scapy.Ether())
+
+
+    #Para no imprimir todo el summary de las respuestas T1
+    #se hace lo siguiente.
+
+    for element in answered_list:
+        # Cada element es una lista de las respuestas (packet sent, answered) por
+        # Ello se procede a solo imprimir el elemento de respuesta [1] aunque imprimira objetos
+        # los cuales no tienen sentido por ello se procede a ir al metodo show
+        # Igual se puede ir al metodo psr para imprimir la ip address del objetivo
+        # Con hwsrc se imprime la MAC address del objetivo
+
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("--------------------------------")
 
 #La funcion scapy.arping puede bsucar dentro de un rango
 scan("10.0.2.2/24")
