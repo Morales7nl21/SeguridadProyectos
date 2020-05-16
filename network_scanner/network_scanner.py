1e05badf94a40244e4b1e151701140c77d559a1a
Morales7nl21clear <thedjdave98@gmail.com>
2020-05-16 18:23:32 -0500

    optoption

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # Provee la funcion que manda el broadcast pa tener la mac
 import scapy.all as scapy
+import optparse
 
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target to send arp message")
+    (options, arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please especify target, use --help for more info")
+    return options.target
 
-def scan (ip):
+def scan_result(ip):
 
     # Devuelve la mac usando el protocolo ARP se envia un mensaje de tipo broadcast con la ip
     # Para acceder y esta conesta devolviendo la mac.
@@ -14,7 +22,6 @@ def scan (ip):
     # arp_request contiene la instancia del objeto paquete ARP
     # 1 Usando ARP para preguntar quienn es la ip objetivo
     arp_request = scapy.ARP(pdst=ip)
-
     # sow muestra mas informacion
     # arp_request.show()
 
@@ -32,18 +39,13 @@ def scan (ip):
 
     # !! Combinamos los dos objetos
     arp_request_broadcast = broadcast/arp_request
-
-
     # Mandar un paquete con un mac customizado lo permite el metodo srp
     # retorna una pareja de dos listas, el primer elemento de la lista de parejas es
     # (packet sent, answer) y el segundo de la lista son los paquetes sin responder, ya que
     # no nos interesan los unanswered se pueden quitar pero se tiene que referenciar a solo el
     # elemento 1 es decir 0 de la lista.
-
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
-
-
-
+    # Verbose da informacion sobre paquetes enviados, recibidos, e ignorados.
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     # T1 Imprimimos lo que nos dio de la variable combinada el metodo summary.
     # print(answered_list.summary())
     # arp_request_broadcast.show()
@@ -57,17 +59,24 @@ def scan (ip):
 
     #Para no imprimir todo el summary de las respuestas T1
     #se hace lo siguiente.
-
+    client_list = []
     for element in answered_list:
         # Cada element es una lista de las respuestas (packet sent, answered) por
         # Ello se procede a solo imprimir el elemento de respuesta [1] aunque imprimira objetos
         # los cuales no tienen sentido por ello se procede a ir al metodo show
         # Igual se puede ir al metodo psr para imprimir la ip address del objetivo
         # Con hwsrc se imprime la MAC address del objetivo
+        client_dict= {"ip": element[1].psrc, "MAC": element[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list
 
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("--------------------------------")
+def print_result(result_list):
+    print("IP\t\t\t       MAC Address\n-----------------------------------------------")
+    for client in result_list:
+        print(client["ip"]+"\t\t\t"+client["MAC"])
 
-#La funcion scapy.arping puede bsucar dentro de un rango
-scan("10.0.2.2/24")
+
+# La funcion scapy.arping puede bsucar dentro de un rango
+options = get_args()
+scan_result = scan_result(options)
+print_result(scan_result)
