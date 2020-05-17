0d228396780755683917fea97da2a94c12024836
Morales7nl21clear <thedjdave98@gmail.com>
2020-05-17 02:55:52 -0500

    ..

--- arp-spoof/arp-spoof.py
+++ arp-spoof/arp-spoof.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
-packet=scapy.ARP()
+# si op es 1 manda un ARP request
+# si op es 2 manda un ARP response
+
+# psrc nos sirve para fingir que la ip que ocuparemos serà la del router con la mac de esta maquina
+packet=scapy.ARP(op=2,pdst="10.0.2.15", hwdst="08:00:27:e6:e5:59", psrc="10.0.2.1")
